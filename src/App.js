42ec6d306a83ed5583b32bf9694f5e47836d7cee
IanKoech <iankoech9@gmail.com>
2022-01-27 14:29:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import react, {Component} from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Home from './Component/Pages/Home';
+
+//Pages
 import PageWrapper from './Component/PageWrapper';
+import Home from './Component/Pages/Home';
+import About from './Component/Pages/About';
 
 class App extends Component{
   render(){
     return (
-      <PageWrapper>
-        <Router>
+      <Router>
+        <PageWrapper>
           <Routes>
-            <Route path="/home" element={<Home/>}/>
+
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+
           </Routes>
-        </Router>
-      </PageWrapper>
+        </PageWrapper>
+      </Router>
     );
   }
 }
