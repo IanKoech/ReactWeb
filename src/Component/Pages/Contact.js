e31aac72d41d0232765ff3291957de5e5a52183f
IanKoech <iankoech9@gmail.com>
2022-06-24 18:06:41 +0300

    Added formuk

--- src/Component/Pages/Contact.js
+++ src/Component/Pages/Contact.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import Field from '../Common/Field';
 
-const fields = [
-    {name:"name", elementName:"input", type:"text", placeholder:"Your name"},
-    {name:"email", elementName:"input", type:"email", placeholder:"Your email"},
-    {name:"phone", elementName:"input", type:"text", placeholder:"Your phone number"},
-    {name:"message", elementName:"textarea", type:"text", placeholder:"Type your message"}
-];
+const fields = {
+    sections : [
+        [
+            {name:"name", elementName:"input", type:"text", placeholder:"Your name"},
+            {name:"email", elementName:"input", type:"email", placeholder:"Your email"},
+            {name:"phone", elementName:"input", type:"text", placeholder:"Your phone number"},
+        ],
+        [
+            {name:"message", elementName:"textarea", type:"text", placeholder:"Type your message"}
+        ]
+    ]
+};
 
 class Contact extends Component{
     constructor(props){
@@ -18,6 +24,11 @@ class Contact extends Component{
             message:""
         }
     }
+
+    submitForm(e){
+        alert("The form was successfully submitted");
+    }
+
     render(){
         return (
             <section className="page-section" id="contact">
@@ -29,39 +40,23 @@ class Contact extends Component{
             
                 <form id="contactForm" data-sb-form-api-token="API_TOKEN">
                     <div className="row align-items-stretch mb-5">
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                
-                                <input className="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required"
-                                  value={this.state.name} onChange={e => this.setState({name: e.target.value})}
-                                 />
-                                <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                            </div>
-                            <div className="form-group">
-                               
-                                <input className="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email"
-                                  value={this.state.email} onChange={e => this.setState({email: e.target.value})}
-                                />
-                                <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                            </div>
-                            <div className="form-group mb-md-0">
-                             
-                                <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" 
-                                  value={this.state.phone} onChange={e => this.setState({phone: e.target.value})}
-                                />
-                                <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                            </div>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group form-group-textarea mb-md-0">
-                                
-                                <textarea className="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"  
-                                  value={this.props.message} onChange={e => this.setState({message: e.target.value})}
-                                />
-                                <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                            </div>
-                        </div>
+                        {fields.sections.map((section, sectionIndex) => {
+                            console.log("Rendering section ",sectionIndex, " with ",section);
+                            return (
+                                <div className='col-md-6' key={sectionIndex}>
+                                    {section.map((field, i) => {
+                                        return <Field 
+                                           {...field}
+                                           key={i}
+                                           value={this.state[field.name]}
+                                           onChange={e => this.setState({
+                                               [field.name]: e.target.value
+                                            })}
+                                        />
+                                    })}
+                                </div>
+                            )
+                        })}
                     </div>
                    
                     <div className="d-none" id="submitSuccessMessage">
@@ -75,7 +70,13 @@ class Contact extends Component{
                     
                     <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
                      
-                    <div className="text-center"><button className="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
+                    <div className="text-center">
+                        <button className="btn btn-primary btn-xl text-uppercase enabled" 
+                        id="submitButton" 
+                        type="submit"
+                        onClick={e => this.submitForm(e)}
+                        >Send Message</button>
+                    </div>
                 </form>
             </div>
         </section>
